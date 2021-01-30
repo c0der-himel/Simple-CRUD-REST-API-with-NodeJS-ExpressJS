f67b5bc83e632dcee31e21ad4641165f7b319895
himel <himel777hussain@gmail.com>
2021-01-30 13:51:34 +0600

    node server created

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: http://localhost:${PORT}`);
+});
